34ef339a05210727c40807ca302574018196a8a4
progammerBoy <sy8692935@gmail.com>
2026-06-20 20:21:26 +0530

    change backend api

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -39,7 +39,7 @@ const Contact = () => {
 
       try {
         // --- NODEMAILER API INTEGRATION ---
-        const response = await fetch("https://inquiry-fvgfrs5w.b4a.run/api/contact", {
+        const response = await fetch("https://manageinquiry-x1sr0qrz.b4a.run/api/contact", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
